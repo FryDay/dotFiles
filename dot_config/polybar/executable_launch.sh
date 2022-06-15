9c1a8c24292a2ea62506eeb1234bde38bb947f4d
Jordan Day <jordan@modernhawk.com>
2022-06-15 16:05:24 -0500

    Remove easyeffects, add tmux, fix polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar leftBar &
-polybar rightBar &
+polybar --config=$HOME/.config/polybar/config.ini leftBar &
+polybar --config=$HOME/.config/polybar/config.ini rightBar &
 
 echo "Polybar launched..."
